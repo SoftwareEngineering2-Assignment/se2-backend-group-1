dcc8ff447e1152a5833c80ae207d311d38b3e3da
panospro <panagiotisskaki@yahoo.gr>
2022-12-21 01:03:37 +0200

    Improve Documentation
    Improve the documentation of middlewares

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,7 +1,13 @@
+/*
+* Importing the authorization, error and validation files
+*/
 const authorization = require('./authorization');
 const error = require('./error');
 const validation = require('./validation');
 
+/*
+* This code exports an object with three properties: authorization, error, and validation.
+*/
 module.exports = {
   authorization,
   error,
