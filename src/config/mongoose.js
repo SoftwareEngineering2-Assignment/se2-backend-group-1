7bfcce0705f2870d79d8fc51a5e88d71536bc67b
panospro <panagiotisskaki@yahoo.gr>
2022-12-20 23:51:16 +0200

    Improve the documentation
    Improve the documentation of the config folder

--- src/config/mongoose.js
+++ src/config/mongoose.js
@@ -1,5 +1,11 @@
+/*
+* Ιmport the file mongoose in the object mongoose.
+*/
 const mongoose = require('mongoose');
 
+/*
+* An object containing options for the Mongoose connection is defined.
+*/
 const mongooseOptions = {
   useNewUrlParser: true,
   useCreateIndex: true,
@@ -9,8 +15,16 @@ const mongooseOptions = {
   keepAlive: true,
   keepAliveInitialDelay: 300000
 };
+
+/*
+* mongodbUri takes the value from the .env file
+*/ 
 const mongodbUri = process.env.MONGODB_URI;
-  
+
+/*
+* The above options are passed to the connect() method of the mongoose object when the connection is established.
+* The connect() method takes two arguments: the URI of the MongoDB database (from the .env file) and the options from above.
+*/
 module.exports = () => {
   // eslint-disable-next-line no-console
   mongoose.connect(mongodbUri, mongooseOptions).catch(console.error);
