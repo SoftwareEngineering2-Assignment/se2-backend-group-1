2f32fcb254fbb1234a63cbae5444f324d1c74a2a
panospro <panagiotisskaki@yahoo.gr>
2022-12-21 05:14:16 +0200

    Extra Documentation

--- src/routes/root.js
+++ src/routes/root.js
@@ -1,11 +1,30 @@
+/*
+* Import path and express 
+*/
 const express = require('express');
 const path = require('path');
 
+/*
+* Define a middleware router object. 
+*/
 const router = express.Router();
 
+/*
+* Set the path of current module to index.html.  
+*/
 const file = path.join(__dirname, '../../index.html');
+
+/*
+* Make the middleware function to serve static files from the same directory as the index.html file.
+*/
 router.use(express.static(file));
 
+/*
+* Send the contents of the index.html file as a response to the client.
+*/
 router.get('/', (req, res) => res.sendFile(file));
 
+/*
+* Export the router object, which makes it available for use in other parts of the application.
+*/
 module.exports = router;
--- src/utilities/index.js
+++ src/utilities/index.js
@@ -1,3 +1,6 @@
+/*
+* Import mailer from the same folder and export an object with properties, mainer (with value mailer).
+*/
 const mailer = require('./mailer');
 
 module.exports = {
