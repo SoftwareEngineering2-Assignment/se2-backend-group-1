d6e0b3cb0a20c18379aa44518de93a6c8bb94abb
panospro <panagiotisskaki@yahoo.gr>
2022-12-22 17:28:08 +0200

    Update sources.js

--- src/routes/sources.js
+++ src/routes/sources.js
@@ -9,14 +9,13 @@ const router = express.Router();
 const Source = require('../models/source');
 
 /* 
-* Every handling of GET/POST requests of different types and using the authorization middleware first.
-* If an error occurs during the execution of the following routes handlers, it will pass control to the next middleware with the error 
-* as an argument, for the error to be handled.
+* Every handling of GET/POST requests of different types and using the authorization middleware first. Then send a JSON object with the appropriate 
+* response. If an error occurs during the execution of the following routes handlers, it will pass control to the next middleware with the error 
+* as an argument, for the error to be handled. 
 */
 
 /*
 * Retrieve all sources that belong to a user, then go through the sources and pushes an object containing the source information.
-* Then return a JSON object with success property and the sources array.
 */  
 router.get('/sources',
   authorization,
@@ -48,9 +47,7 @@ router.get('/sources',
   });
   
 /*
-* Check if a source with the same name already exists for the user. If it does, return a response with status code of 409 and a message indicating
-* that a source with that name already exists. If it does not, create a new Source object with the provided data and the id of the authenticated
-* user and saves it to the database. If the save is successful, return a response with success set to true.
+* Check if a source with the same name already exists for the user.
 */
 router.post('/create-source', 
   authorization,
@@ -82,11 +79,7 @@ router.post('/create-source',
   }); 
   
 /*
-* Check if the source object with the provided id exists and if it is owned by the authenticated user. If it does not or is not owned by 
-* the authenticated user, the route sends a response with a 409 status code and an error message. If it does, check if there is already 
-* a source object with the same name owned by the authenticated user and if it does exist, send a response with status set to 409 and an error message.
-* If there are no issues, update the properties of the source object with the provided values and save the updated object to the database. 
-* Then send a response with success set to true.
+* Check if the source object with the provided id exists and if it is owned by the authenticated user.
 */
 router.post('/change-source', 
   authorization,
@@ -124,10 +117,7 @@ router.post('/change-source',
   }); 
 
 /*
-* Delete a source if it is found in the database and the authenticated user is the owner of the source. If it is not found or the authenticated
-* user is not the owner of the source, return a JSON object with status set to 409 and a message. If the source is successfully deleted,
-* return a JSON object with success set to true. Find a single source in the database that matches the specified id and owner s and remove it. 
-* Return the deleted document as a result, if no document is found, return null.
+* Delete a source if it is found in the database and the authenticated user is the owner of the source.
 */
 router.post('/delete-source', 
   authorization,
@@ -151,9 +141,7 @@ router.post('/delete-source',
 /*
 * Get the name, owner and user from body. Then check if the owner is 'self'. If it is, set the userId to the id of the user object. 
 * If it is not, set the userId to the value of owner. Use the Source model to find a data source with a name and owner that match the
-* provided name and userId, respectively. If no matching source is found, return a response with status of 409 and a message indicating 
-* that the source was not found. If a matching source is found, create a new object called source and populate it with the type, url, login,
-* passcode and vhost of the found source. Then return a response with success set to true and the source object.
+* provided name and userId, respectively.
 */
 router.post('/source',
   async (req, res, next) => {
@@ -188,7 +176,6 @@ router.post('/source',
 * Check a list of body and create new sources if they don't already exist in the database. Initialize an empty array to store any sources 
 * that need to be created. Then loop through the list in body and check if each one exists in the database. If it doesn't exist, add to the 
 * newSources array. After the loop completes, loop through the newSources and create a new source for each item in the array using the Source model.
-* Return a JSON response containing the success set to true and newSources.
 */
 router.post('/check-sources',
   authorization,
