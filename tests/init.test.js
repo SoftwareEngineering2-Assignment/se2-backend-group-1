db610e3f9d9b632a34f13745abd9769524ea7fd7
panospro <panagiotisskaki@yahoo.gr>
2022-12-30 13:27:55 +0200

    Update init.test.js

--- tests/init.test.js
+++ tests/init.test.js
@@ -3,6 +3,7 @@
 * Import .env, node:http, ava, got, test-listen and helpers from utilities for the tests.
 */ 
 require('dotenv').config();
+
 const http = require('node:http');
 const test = require('ava').default;
 const got = require('got');
@@ -21,69 +22,15 @@ test.after.always((t) => {
   t.context.server.close();
 });
 
-// test('GET /statistics returns correct response and status code', async (t) => {
-//   const {body, statusCode} = await t.context.got('general/statistics');
-//   t.is(body.sources, 0);
-//   t.assert(body.success);
-//   t.is(statusCode, 200);
-// });
-
-// test('GET /sources returns correct response and status code', async (t) => {
-//   const token = jwtSign({id: 1});
-//   const {statusCode} = await t.context.got(`sources/sources?token=${token}`);
-//   t.is(statusCode, 200);
-// });
-
-
-
-
-
-
-
-
-
-
-
-test.beforeEach(async t => {
-  // Create a dashboard
-  t.context.dashboard = new dashboard({name:'Test Dashboard',views: 0,owner: mongoose.Types.ObjectId()});
-  await t.context.dashboard.save(); // Save it
-  t.context.token = jwtSign({id: t.context.dashboard.owner});
-});
-
-test.afterEach.always(async t => {
-  // Delete the dashboard 
-  await t.context.dashboard.delete();
-});
-
-test('GET /dashboards returns correct response and status code and type', async t => {
-  const token = t.context.token
-  const {body, statusCode} = await t.context.got(`dashboards/dashboards?token=${token}`);
-
-  t.is(statusCode,200)
+test('GET /statistics returns correct response and status code', async (t) => {
+  const {body, statusCode} = await t.context.got('general/statistics');
+  t.is(body.sources, 0);
   t.assert(body.success);
-  t.is(typeof body.dashboards, 'object');
-  t.is(body.dashboards.length, 1);
-});
-
-test('GET /dashboards with no dashboards return correct response and status code and type', async t => {
-  // Create one empty dashboard to see if it's accurate
-  const token = jwtSign({id: mongoose.Types.ObjectId()});
-  const {body, statusCode} = await t.context.got(`dashboards/dashboards?token=${token}`);
-
   t.is(statusCode, 200);
-  t.assert(body.success);
-  t.is(typeof body.dashboards, 'object');
-  t.is(body.dashboards.length, 0);
 });
 
-
-// test('GET /dashboards throws error on invalid token', async t => {
-//   const token = jwtSign({id: 'invalid-id'});
-//   const error = await t.throwsAsync(async () => {
-//     await t.context.got(`dashboards/dashboards?token=${token}`, { throwHttpErrors: true });
-//   });
-//   console.log(error);
-//   t.is(error.statusCode, 500);
-//   t.truthy(error.response.body.error);
-// });
+test('GET /sources returns correct response and status code', async (t) => {
+  const token = jwtSign({id: 1});
+  const {statusCode} = await t.context.got(`sources/sources?token=${token}`);
+  t.is(statusCode, 200);
+});
