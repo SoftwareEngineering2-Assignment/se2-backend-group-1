74cde07154dbe4db67884ed77a8d403ddffe85bb
MarshallRNH2 <58854172+MarshallRNH2@users.noreply.github.com>
2023-01-15 17:59:23 +0200

    Update routes_users.test.js

--- tests/routes_users.test.js
+++ tests/routes_users.test.js
@@ -56,11 +56,90 @@ test('POST /create Error creating a user with existing email', async t => {
 });
 
 
-// Test for /authenticate to authenticate a user
-// test('POST /authenticate Authenticate the user', async t => {
-//     const sourceJson = {json: {username: 'username', password: 'password'}};
-//     const {body, statusCode} = await t.context.got.post(`users/authenticate?`, sourceJson);
+//Test for /authenticate to authenticate a user
+test('POST /authenticate Authenticate the user', async t => {
+    const sourceJson = {json: {username: 'username', password: 'password'}};
+    const {body, statusCode} = await t.context.got.post(`users/authenticate?`, sourceJson);
 
-//     t.is(statusCode, 200);
-//     t.assert(body.token);
-// });
+    t.is(statusCode, 200);
+    t.assert(body.token);
+});
+
+//Test for /authenticate user not found
+test('POST /authenticate user not found', async t => {
+    const sourceJson = {json: {username: -1, password: 'password'}};
+    const {body, statusCode} = await t.context.got.post(`users/authenticate?`, sourceJson);
+    t.is(statusCode, 200);
+    t.deepEqual(body, {status: 401,
+    message: 'Authentication Error: User not found.'});
+});
+
+//Test for /authenticate password not match
+test('POST /authenticate password not match', async t => {
+    const sourceJson = {json: {username: 'username', password: "-1-1-1"}};
+    const {body, statusCode} = await t.context.got.post(`users/authenticate?`, sourceJson);
+    t.is(statusCode, 200);
+    t.deepEqual(body, {
+        status: 401,
+        message: 'Authentication Error: Password does not match!'
+      });
+});
+
+
+//Test for /resetpassword to reset password
+test('POST /resetpassword to reset password', async t => {
+    const sourceJson = {json: {username: 'username', password: "newpassword"}};
+    const {body, statusCode} = await t.context.got.post(`users/resetpassword?`, sourceJson);
+    t.is(statusCode, 200);
+    t.deepEqual(body, {
+        ok: true,
+        message: 'Forgot password e-mail sent.'
+      });
+});
+
+//Test for /resetpassword user not found
+test('POST /resetpassword user not found', async t => {
+    const sourceJson = {json: {username: -1, password: "newpassword"}};
+    const {body, statusCode} = await t.context.got.post(`users/resetpassword?`, sourceJson);
+    t.is(statusCode, 200);
+    t.deepEqual(body, {
+        status: 404,
+        message: 'Resource Error: User not found.'
+      });
+});
+
+//Test for /changepassword for change password
+test('POST /changepassword for change password', async t => {
+    const sourceJson = {json: {username: t.context.user.username, password: t.context.user.password}};
+    t.context.token = jwtSign({username: t.context.user.username, password: t.context.user.password, email: t.context.user.email});
+    const {body, statusCode} = await t.context.got.post(`users/changepassword?token=${t.context.token}`, sourceJson);
+    t.is(statusCode, 200);
+    t.deepEqual(body, {
+        ok: true,
+        message: 'Password was changed.'
+      });
+});
+
+//Test for /changepassword user not found
+test('POST /changepassword user not found', async t => {
+    const sourceJson = {json: {username: t.context.user.username, password: t.context.user.password}};
+    t.context.token = jwtSign({id: -1});
+    const {body, statusCode} = await t.context.got.post(`users/changepassword?token=${t.context.token}`, sourceJson);
+    t.is(statusCode, 200);
+    t.deepEqual(body, {
+    status: 404,
+    message: 'Resource Error: User not found.'
+  });
+});
+
+//Test for /changepassword token has expired
+test('POST /changepassword token has expired', async t => {
+    const sourceJson = {json: {username: -1, password: t.context.user.password}};
+    t.context.token = jwtSign({username: t.context.user.username, password: t.context.user.password, email: t.context.user.email});
+    const {body, statusCode} = await t.context.got.post(`users/changepassword?token=${t.context.token}`, sourceJson);
+    t.is(statusCode, 200);
+    t.deepEqual(body, {
+        status: 410,
+        message: ' Resource Error: Reset token has expired.'
+      });
+});
