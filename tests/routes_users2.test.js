353fe6f2252b05ed102086a723ef23713fe32aac
panospro <panagiotisskaki@yahoo.gr>
2023-01-17 19:32:50 +0200

    Update routes_users2.test.js

--- tests/routes_users2.test.js
+++ tests/routes_users2.test.js
@@ -28,25 +28,40 @@ test.afterEach.always(async t => {
     await User.deleteMany({});
 });
 
-//Test for /resetpassword user not found
-test('POST /resetpassword user not found', async t => {
-    const sourceJson = {json: {username: -1, password: "newpassword"}};
-    const {body, statusCode} = await t.context.got.post(`users/resetpassword?`, sourceJson);
+
+// Tests for /create for a new user
+test('POST /create Create new user', async t => {
+    const sourceJson = {json: {username: 'newuser', password: 'newpassword', email: 'newuser@example.com'}};
+    const {body, statusCode} = await t.context.got.post(`users/create?`, sourceJson);
     t.is(statusCode, 200);
-    t.deepEqual(body, {
-        status: 404,
-        message: 'Resource Error: User not found.'
-      });
+    t.assert(body.success);
+    t.assert(body.id);
+
+    // Find the user with that name and password
+    // const user = await User.findOne({username: 'newuser'}).select('+password');
+    // t.assert(user);
+    // t.is(user.email, 'newuser@example.com');
 });
 
+// //Test for /resetpassword user not found
+// test('POST /resetpassword user not found', async t => {
+//     const sourceJson = {json: {username: -1, password: "newpassword"}};
+//     const {body, statusCode} = await t.context.got.post(`users/resetpassword?`, sourceJson);
+//     t.is(statusCode, 200);
+//     t.deepEqual(body, {
+//         status: 404,
+//         message: 'Resource Error: User not found.'
+//       });
+// });
 
-// Test for /resetpassword to reset password
-test('POST /resetpassword to reset password', async (t) => {
-    const sourceJson = {json: {username: 'username'}};
-    const {body, statusCode} = await t.context.got.post(`users/resetpassword?`, sourceJson);
-    t.is(statusCode, 200);
-    t.deepEqual(body, {
-        ok: true,
-        message: 'Forgot password e-mail sent.'
-      });
-});
+
+// // Test for /resetpassword to reset password
+// test('POST /resetpassword to reset password', async (t) => {
+//     const sourceJson = {json: {username: 'username'}};
+//     const {body, statusCode} = await t.context.got.post(`users/resetpassword?`, sourceJson);
+//     t.is(statusCode, 200);
+//     t.deepEqual(body, {
+//         ok: true,
+//         message: 'Forgot password e-mail sent.'
+//       });
+// });
