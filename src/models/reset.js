7e7982b49a9dbe055b225fff70803522ff9cbd63
panospro <panagiotisskaki@yahoo.gr>
2023-02-23 02:20:39 +0200

    Update reset.js

--- src/models/reset.js
+++ src/models/reset.js
@@ -4,7 +4,24 @@ const beautifyUnique = require('mongoose-beautiful-unique-validation');
 const {constants: {expires}} = require('../utilities/validation');
 
 // Define a Mongoose schema for a reset token document.
-const ResetSchema = new mongoose.Schema({username: {index: true, type: String, required: true, unique: 'A token already exists for that username!', lowercase: true}, token: {type: String, required: true}, expireAt: {type: Date, default: Date.now, index: { expires }}}); // Set a TTL index on the expireAt field to automatically delete expired tokens.
+const ResetSchema = new mongoose.Schema({
+  username: {
+    index: true, // Adds faster performance
+    type: String,
+    required: true,
+    unique: 'A token already exists for that username!',
+    lowercase: true
+  },
+  token: {
+    type: String,
+    required: true,
+  },
+  expireAt: {
+    type: Date,
+    default: Date.now,
+    index: { expires }, // Set a TTL index on the expireAt field to automatically delete expired tokens.
+  },
+});
 
 // Format unique constraint errors in a user-friendly way.
 ResetSchema.plugin(beautifyUnique);
